0c312bf63cc0399d1db88b46381e7f64ec9216f2
Jonathan Stern <jstern718@gmail.com>
2023-07-05 20:21:17 -0400

    cron job works

--- script.py
+++ script.py
@@ -1,16 +1,17 @@
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 import slack
 import os
 from dotenv import load_dotenv
-from pathlib import Path
 
-env_path = Path('.') / '.env'
+load_dotenv()
 user_id = 'U04SD2EFKM5'
-
-load_dotenv(dotenv_path=env_path)
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+SLACK_TOKEN = os.environ["SLACK_TOKEN"]
+print("SLACK_TOKEN", SLACK_TOKEN)
+client = WebClient(token=os.environ['SLACK_TOKEN'])
 
 def message_instructor():
-    try: 
+    try:
         result = client.chat_postMessage(
             channel= user_id,
             text= 'Hello World'
